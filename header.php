1b32941d003021cf10a919beceaa86b29e39bc03
oakwoodgates <wpguru4u@gmail.com>
2024-02-29 14:10:08 -0500

    header

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,8 +13,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -21,37 +23,42 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'oakwood' ); ?></a>
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'oakwood'); ?></a>
 
-	<header id="masthead" class="site-header">
-		<nav id="site-navigation" class="main-navigation navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Offcanvas navbar large">
-			<div class="container-xl">
-				<div class="navbar-brand">
-					<?php the_custom_logo(); ?>
-				</div>
-				<button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2" aria-controls="offcanvasNavbar2" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasNavbar2" aria-labelledby="offcanvasNavbar2Label">
-					<div class="offcanvas-header">
-						<h5 class="offcanvas-title" id="offcanvasNavbar2Label">Offcanvas</h5>
-						<button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+		<header id="masthead" class="site-header">
+
+			<nav id+"site-navigation" class="main-navigation navbar navbar-expand-md navbar-dark bg-dark" aria-label="Offcanvas navbar large">
+				<div class="container">
+					<div class="navbar-brand">
+						<?php the_custom_logo(); ?>
 					</div>
-					<div class="offcanvas-body">
-						<ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-							<?php
-							wp_nav_menu(
-								array(
-									'theme_location' => 'menu-1',
-									'menu_id'        => 'primary-menu',
-								)
-							);
-							?>
-						</ul>
+					<button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar3" aria-controls="offcanvasNavbar3" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasNavbar3" aria-labelledby="offcanvasNavbar3Label">
+						<div class="offcanvas-header">
+							<h5 class="offcanvas-title" id="offcanvasNavbar3Label">Offcanvas</h5>
+							<button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+						</div>
+						<div class="offcanvas-body">
+							<ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+								<?php
+								wp_nav_menu(
+									[
+										'fallback_cb'    => false,
+										'theme_location' => 'menu-1',
+										'menu_id'        => 'primary-menu',
+										'menu_class'     => 'navbar-nav',
+										'container'      => false,
+										'walker' => new BS5_Nav_Menu()
+									]
+								);
+								?>
+							</ul>
+						</div>
 					</div>
 				</div>
-			</div>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+			</nav>
+		</header><!-- #masthead -->
